f35d7bfab08d1e41af7c180b54b03334711ca6cb
flare <flare2399@gmail.com>
2020-08-20 02:15:52 +0100

    Add current command to check current pokemon.

--- pokecord/general.py
+++ pokecord/general.py
@@ -31,7 +31,11 @@ class GeneralMixin(MixinMeta):
         """List a trainers or your own pokémon!"""
         conf = await self.user_is_global(ctx.author)
         if not await conf.has_starter():
-            return await ctx.send(_("You haven't picked a starter pokemon yet! Check out {prefix} before trying to list your pokemon.").format(prefix=ctx.clean_prefix))
+            return await ctx.send(
+                _(
+                    "You haven't picked a starter pokemon yet! Check out {prefix} before trying to list your pokemon."
+                ).format(prefix=ctx.clean_prefix)
+            )
         user = user or ctx.author
         async with ctx.typing():
             result = self.cursor.execute(SELECT_POKEMON, (user.id,)).fetchall()
@@ -59,11 +63,17 @@ class GeneralMixin(MixinMeta):
         This is found at the bottom of the pokemon on `[p]list`"""
         conf = await self.user_is_global(ctx.author)
         if not await conf.has_starter():
-            return await ctx.send(_("You haven't picked a starter pokemon yet! Check out {prefix} before trying to nickname a pokemon.").format(prefix=ctx.clean_prefix))
+            return await ctx.send(
+                _(
+                    "You haven't picked a starter pokemon yet! Check out {prefix} before trying to nickname a pokemon."
+                ).format(prefix=ctx.clean_prefix)
+            )
         if id <= 0:
             return await ctx.send(_("The ID must be greater than 0!"))
         if len(nickname) > 40:
-            await ctx.send("The nickname you have specified is too big. It must be under 40 characters.")
+            await ctx.send(
+                "The nickname you have specified is too big. It must be under 40 characters."
+            )
             return
         async with ctx.typing():
             result = self.cursor.execute(SELECT_POKEMON, (ctx.author.id,),).fetchall()
@@ -95,7 +105,11 @@ class GeneralMixin(MixinMeta):
         """Release a pokémon."""
         conf = await self.user_is_global(ctx.author)
         if not await conf.has_starter():
-            return await ctx.send(_("You haven't picked a starter pokemon yet! Check out {prefix} before trying to release a pokemon.").format(prefix=ctx.clean_prefix))
+            return await ctx.send(
+                _(
+                    "You haven't picked a starter pokemon yet! Check out {prefix} before trying to release a pokemon."
+                ).format(prefix=ctx.clean_prefix)
+            )
         if id <= 0:
             return await ctx.send(_("The ID must be greater than 0!"))
         async with ctx.typing():
@@ -291,6 +305,80 @@ class GeneralMixin(MixinMeta):
                 await ctx.send(embed=embeds[0])
                 return
             await menu(ctx, embeds, DEFAULT_CONTROLS)
-            
+
     @commands.command()
-    async def current(self, ctx)
+    @commands.max_concurrency(1, commands.BucketType.user)
+    async def current(self, ctx):
+        """Show your current selected pokemon"""
+        conf = await self.user_is_global(ctx.author)
+        if not await conf.has_starter():
+            return await ctx.send(
+                _(
+                    "You haven't picked a starter pokemon yet! Check out {prefix} before trying to list your pokemon."
+                ).format(prefix=ctx.clean_prefix)
+            )
+        user = ctx.author
+        async with ctx.typing():
+            result = self.cursor.execute(SELECT_POKEMON, (user.id,)).fetchall()
+        pokemons = []
+        for i, data in enumerate(result, start=1):
+            poke = json.loads(data[0])
+            poke["sid"] = i
+            pokemons.append(poke)
+        if not pokemons:
+            return await ctx.send(_("You don't have any pokémon, go get catching trainer!"))
+        _id = await conf.pokeid()
+        try:
+            pokemon = pokemons[_id - 1]
+        except IndexError:
+            await ctx.send(
+                _(
+                    "An error occured trying to find your pokemon at slot {slotnum}\nAs a result I have set your default pokemon to 1."
+                ).format(slotnum=_id)
+            )
+            await conf.pokeid.set(1)
+            return
+        else:
+            stats = pokemon["stats"]
+            pokestats = tabulate.tabulate(
+                [
+                    [_("HP"), stats["HP"]],
+                    [_("Attack"), stats["Attack"]],
+                    [_("Defence"), stats["Defence"]],
+                    [_("Sp. Atk"), stats["Sp. Atk"]],
+                    [_("Sp. Def"), stats["Sp. Def"]],
+                    [_("Speed"), stats["Speed"]],
+                ],
+                headers=[_("Stats"), _("Value")],
+            )
+            nick = pokemon.get("nickname")
+            alias = _("**Nickname**: {nick}\n").format(nick=nick) if nick is not None else ""
+            variant = (
+                _("**Variant**: {variant}\n").format(variant=pokemon.get("variant"))
+                if pokemon.get("variant")
+                else ""
+            )
+            types = ", ".join(pokemon["type"])
+            desc = _(
+                "**ID**: {id}\n{alias}**Level**: {level}\n**Type**: {type}\n**XP**: {xp}/{totalxp}\n{variant}{stats}"
+            ).format(
+                id=f"#{pokemon.get('id')}" if pokemon.get("id") else "0",
+                alias=alias,
+                level=pokemon["level"],
+                type=types,
+                variant=variant,
+                xp=pokemon["xp"],
+                totalxp=self.calc_xp(pokemon["level"]),
+                stats=box(pokestats, lang="prolog"),
+            )
+            embed = discord.Embed(
+                title=self.get_name(pokemon["name"], ctx.author), description=desc
+            )
+            if pokemon.get("id"):
+                embed.set_thumbnail(
+                    url=f"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{str(pokemon['id']).zfill(3)}.png"
+                    if not pokemon.get("url")
+                    else pokemon.get("url")
+                )
+            embed.set_footer(text=_("Pokémon ID: {number}").format(number=pokemon["sid"]))
+            await ctx.send(embed=embed)
