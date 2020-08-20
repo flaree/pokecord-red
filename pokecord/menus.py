9fe2bdb877c214ce034d9678c5007a9f2d88fdc5
flare <flare2399@gmail.com>
2020-08-20 02:15:40 +0100

    .

--- pokecord/menus.py
+++ pokecord/menus.py
@@ -140,7 +140,7 @@ class PokeList(menus.ListPageSource):
             if pokemon.get("variant")
             else ""
         )
-        types = ", ".join(pokemon['type'])
+        types = ", ".join(pokemon["type"])
         desc = _(
             "**ID**: {id}\n{alias}**Level**: {level}\n**Type**: {type}\n**XP**: {xp}/{totalxp}\n{variant}{stats}"
         ).format(
