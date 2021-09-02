1daa8fab2312f74197a2a4d9d87b78f8dfd8a32b
qenu <xchedeiklo@gmail.com>
2021-09-02 18:13:54 +0800

    test starter

--- pokecord/pokecord.py
+++ pokecord/pokecord.py
@@ -322,29 +322,39 @@ class Pokecord(
             "scorbunny": self.pokemondata[743],
             "sobble": self.pokemondata[746],
         }
-        starter_translated = [
-            name.lower()
-            for pokemon in starter_pokemon.values()
-            for name in pokemon["name"].values()
-        ]
-        if pokemon.lower() not in starter_translated:
-            await ctx.send(_("That's not a valid starter pokémon, trainer!"))
-            return
+        # starter_translated = [
+        #     name.lower()
+        #     for pokemon in starter_pokemon.values()
+        #     for name in pokemon["name"].values()
+        # ]
+        # if pokemon.lower() not in starter_translated:
+        #     await ctx.send(_("That's not a valid starter pokémon, trainer!"))
+        #     return
+        for starter in starter_pokemon.values():
+            if pokemon.lower() in self.get_pokemon_name(starter):
+                pokemon = starter
+        else:
+            return await ctx.send(_("That's not a valid starter pokémon, trainer!"))
+
+        await ctx.send("hey")
+
         await ctx.send(
             _("You've chosen {pokemon} as your starter pokémon!").format(pokemon=pokemon.title())
         )
 
         # pokemon = starter_pokemon[pokemon.lower()
-        supported_languages = len(
-            list(starter_pokemon.values())[0]["name"].values()
-        )  # number of languages
-        starter_index = int(
-            starter_translated.index(pokemon.lower()) / supported_languages
-        )  # get location of name
-        pokemon = starter_pokemon[
-            list(starter_pokemon.keys())[starter_index]
-        ]  # get starter by index
+        # supported_languages = len(
+        #     list(starter_pokemon.values())[0]["name"].values()
+        # )  # number of languages
+        # starter_index = int(
+        #     starter_translated.index(pokemon.lower()) / supported_languages
+        # )  # get location of name
+        # pokemon = starter_pokemon[
+        #     list(starter_pokemon.keys())[starter_index]
+        # ]  # get starter by index
 
+
+        return
         pokemon["level"] = 1
         pokemon["xp"] = 0
         pokemon["ivs"] = {
