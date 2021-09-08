79334b535fddec8886d26743701e369ab38e1173
qenu <xchedeiklo@gmail.com>
2021-09-08 08:24:40 +0800

    i got this

--- pokecord/dev.py
+++ pokecord/dev.py
@@ -204,4 +204,6 @@ class Dev(MixinMeta):
         except Exception as err:
             return await ctx.send("Unexpected Error: {err}")
 
+        data = json.load(data)
+        await ctx.send(type(data))
         await ctx.send(data)
