a70092e57dfc60f729756e9be26b9d0b1302d191
qenu <xchedeiklo@gmail.com>
2021-09-08 08:09:39 +0800

    last time i swear, im special

--- pokecord/dev.py
+++ pokecord/dev.py
@@ -174,8 +174,7 @@ class Dev(MixinMeta):
         """Shows raw info for an owned pokemon"""
         if user is None:
             user = ctx.author
-        if isinstance(pokemon := await self.get_pokemon(ctx, user=user, pokeid=pokeid), list):
-            await ctx.send("type not list")
+        if not isinstance(pokemon := await self.get_pokemon(ctx, user=user, pokeid=pokeid), list):
             return
         await ctx.send(f"type is list\nDoes it work? {pokemon}")
 
